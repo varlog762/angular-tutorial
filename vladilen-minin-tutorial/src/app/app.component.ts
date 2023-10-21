35a93830b25d36b7eca7ac6a4d6a01cbc92f2866
Gregory Pereverzev <greg.pereverzev@gmail.com>
2023-10-21 16:36:22 +0300

    feat: add Product service

--- vladilen-minin-tutorial/src/app/app.component.ts
+++ vladilen-minin-tutorial/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { IProduct } from './models/product';
+import { products as data } from './data/products';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular app';
+
+  products: IProduct[] = data;
 }
--- vladilen-minin-tutorial/src/app/components/product/product.component.ts
+++ vladilen-minin-tutorial/src/app/components/product/product.component.ts
@@ -1,8 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { IProduct } from 'src/app/models/product';
 
 
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
 })
-export class ProductCOmponent {};
+export class ProductCOmponent {
+  @Input() product: IProduct;
+
+  details = false;
+};
