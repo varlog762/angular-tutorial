abb0020cc8e940320d41aecd9ec8bbbf7a6115cf
Gregory Pereverzev <greg.pereverzev@gmail.com>
2023-10-21 11:06:09 +0300

    feat: rewriten constructor in HomeComponent

--- first-app/src/app/home/home.component.ts
+++ first-app/src/app/home/home.component.ts
@@ -36,8 +36,10 @@ export class HomeComponent {
   filteredLocationList: HousingLocation[] = [];
 
   constructor() {
-    this.housingLocationList = this.housingService.getAllHousingLocations();
-    this.filteredLocationList = this.housingLocationList;
+    this.housingService.getAllHousingLocations().then((housingLocationList: HousingLocation[]) => {
+      this.housingLocationList = housingLocationList;
+      this.filteredLocationList = housingLocationList;
+    });
   }
 
   filterResults(text: string) {
