ac460d723e7ee018fcd5e05fdbc996633d49e746
Gregory Pereverzev <greg.pereverzev@gmail.com>
2023-10-22 16:49:11 +0300

    feat: add onSelected method to HeroComponent

--- tour-of-heroes/src/app/heroes/heroes.component.ts
+++ tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -8,10 +8,10 @@ import { HEROES } from '../mock-heroes';
   styleUrls: ['./heroes.component.scss']
 })
 export class HeroesComponent {
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm',
-  }
-
   heroes = HEROES;
+
+  selectedHero?: Hero;
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
